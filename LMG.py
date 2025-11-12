d0ffd8c8479c0d8fb603f5e8329bc699cadc2c86
RAJA-HACKER-PRO 1ST <anchalraja283@gmail.com>
2025-11-13 00:04:04 +0530

    Update LMG.py

--- LMG.py
+++ LMG.py
@@ -1,10 +1,3 @@
-import platform
-
-arch = platform.architecture()[0]
-
-if arch == "32bit":
-    from LMG_32bit import approval
-else:
-    from LMG_64bit import approval
+from LMG_32bit import approval
 
 approval()
