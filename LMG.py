ca471d79e52506974ce1b37a16824f4e549be3cc
RAJA-HACKER-PRO 1ST <anchalraja283@gmail.com>
2025-11-12 21:44:58 +0530

    Add files via upload

--- LMG.py
+++ LMG.py
@@ -1,2 +1,10 @@
-from LMG import approval
+import platform
+
+arch = platform.architecture()[0]
+
+if arch == "32bit":
+    from LMG_32bit import approval
+else:
+    from LMG_64bit import approval
+
 approval()
